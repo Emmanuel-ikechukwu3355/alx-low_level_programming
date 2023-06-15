37b2beec7c8a4bfe5e082f27d01ed9d6a5c54f8e
Emmanuel-ikechukwu3355 <ikechukwuemmanuel427@gmail.com>
2023-06-15 12:28:50 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <studio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
