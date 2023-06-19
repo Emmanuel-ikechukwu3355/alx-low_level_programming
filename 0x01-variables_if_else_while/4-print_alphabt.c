0e3aedf9e241f16653be60b787f30df40e729ef4
Emmanuel-ikechukwu3355 <ikechukwuemmanuel427@gmail.com>
2023-06-19 13:28:39 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: All the time 0 (success)
  */
+
 int main(void)
 {
 	char k;
 
-	for (k = 'a'; k <= 'z'; k++)
-	{
-		if (k == 'q' || k 'e')
-		{
-			continue;
-		}
-		putchar(k);
-	}
-	putchar('\n');
-	return (0);
+for (k = 'a'; k <= 'z'; k++)
+{
+if (k == 'q' || k 'e')
+{
+continue;
+}
+putchar(k);
+}
+putchar('\n');
+return (0);
 }
