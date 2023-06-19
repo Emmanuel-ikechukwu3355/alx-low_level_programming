39775cc2443973bd99863ccc1e9409ad2757837d
Emmanuel-ikechukwu3355 <ikechukwuemmanuel427@gmail.com>
2023-06-19 13:51:03 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,11 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-
 /**
  * main - Prints a text according number
- *
  * Return: Always 0 (success)
  */
 int main(void)
-
 {
 	int n;
 	int k;
