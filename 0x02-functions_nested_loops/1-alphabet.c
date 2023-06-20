7f56ff035b2b10413d2535961729b4eda2f3a329
Emmanuel-ikechukwu3355 <ikechukwuemmanuel427@gmail.com>
2023-06-20 09:16:33 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * printf_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+}
