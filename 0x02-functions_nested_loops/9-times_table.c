4575dbae072fdd6dc448f2f6a0ec93ff24455099
Emmanuel-ikechukwu3355 <ikechukwuemmanuel427@gmail.com>
2023-06-21 15:46:46 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 {
 int i, j;
 
-for (i = 0; i < = 9; i++)
+for (i = 0; i <= 9; i++)
 {
-for (j = 0; j < = 9; j++)
+for (j = 0; j <= 9; j++)
 {
 if (j != 0)
 printf("%2d", i * j);
