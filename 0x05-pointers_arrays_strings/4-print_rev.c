e6ea6abcaac1954c957d665797219e8399159567
Emmanuel-ikechukwu3355 <ikechukwuemmanuel427@gmail.com>
2023-06-26 03:16:29 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s:string 
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+
+	int 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (0 = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
