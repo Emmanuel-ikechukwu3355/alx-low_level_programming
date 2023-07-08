e3f7ea4085196c9d6c18eff310ee3396f1122328
Emmanuel-ikechukwu3355 <ikechukwuemmanuel427@gmail.com>
2023-07-08 03:04:02 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,14 +14,16 @@ int check_num(char *str)
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str)) /*check if str there are digit*/
-	{
-		return (0);
-	}
+	while (count < strlen(str)) /*count string*/
 
-	count++;
-}
-return (1);
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
 }
 
 /**
@@ -34,25 +36,25 @@ return (1);
 
 int main(int argc, char *argv[])
 {
-	/*declaring variables*/
+	/*Declaring variables*/
 	int count;
 	int str_to_int;
 	int sum = 0;
 
 	count = 1;
-	while (count < argc) /*goes through the whole array*/
+	while (count < argc) /*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
 		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
-
 		/*condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
 			return (1);
+
 		}
 		count++;
 	}
